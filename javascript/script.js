c15253f8ea72c49ee34fd6c290d026b5224c4df3
rajuboyella1753 <rajuboyella737@gmail.com>
2024-07-13 12:20:36 +0530

    Pop-up

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+let popup = document.getElementById("popUp");
+function openPopUp(){
+  popup.classList.add("open-PopUp");
+}
+function closePopUp(){
+  popup.classList.remove("open-PopUp");
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Project 13 PopUp
+![image](/images/title.png)
+
+|Name|Project|Date|Time|
+|---|---|---|---|
+|Raju|13|13/07/2024|12:17|
+|Day : | -|-|8|
